12919e9b3e6b6c1f777ee1a49dc1e98e7f803b15
DrSimaB <calvinbenatha@gmail.com>
2022-09-19 22:47:45 +0200

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *main - swaps the values of two given intergers
+ *swap_int - swaps the values of two given intergers
  *@a: first integer
  *@b: second integer
  *Return : nothing
